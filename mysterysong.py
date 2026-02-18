9846d1d5a0889fbd9c6fec61ff1d69ef2d82cf68
trekswithnick <nicholasbrowningdev@outlook.com>
2026-02-18 19:06:30 +1000

    created game loop

--- mysterysong.py
+++ mysterysong.py
@@ -1,9 +1,24 @@
 class MysterySong:
     def __init__(self, mystery_song):
         self.__mystery_song = mystery_song
+        self.__masked_mystery_song = self.mask_mystery_song(mystery_song)
         
     def get_mystery_song(self):
         return self.__mystery_song
     
     def set_mystery_song(self, mystery_song):
         self.__mystery_song = mystery_song
+        self.__masked_mystery_song = self.mask_mystery_song(mystery_song)
+    
+    def get_masked_mystery_song(self):
+        return self.__masked_mystery_song
+    
+    def mask_mystery_song(self, song: str):
+        result_list = []
+        for char in song:
+            if char.isalnum():
+                result_list.append('_')
+            else:
+                result_list.append(char)
+        return "".join(result_list)
+        
